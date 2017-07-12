2b768959c6336f56af9ef583f0380dac786517da
Luca Colombi <luca@ethicsoft.it>
2017-07-12 12:57:18 +0200

    Update tooltipzoom.js

--- tooltipzoom.js
+++ tooltipzoom.js
@@ -1,4 +1,17 @@
 /*
 Credits: Luca Colombi luca.colombi@ethicsoft.it
 Left this comment while using the plugin
+Requirements: Bootstrap and jQuery
 */
+jQuery.fn.extend({
+	tooltipzoom:function() {
+		return this.each(function() {
+		var self=$(this);
+		if(var src=self.attr(src)){
+			self.tooltip({
+				content: "<img src='"+src+"'>"
+			});
+		});
+	});
+});
+$(function(){$( ".tooltipzoom" ).each(function(){this.tooltipzoom();});});
