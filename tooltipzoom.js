b5823500aef3041a072841ad5b3b2f5bdc64ba8f
Luca Colombi <luca@ethicsoft.it>
2017-07-12 14:23:04 +0200

    Fix

--- tooltipzoom.js
+++ tooltipzoom.js
@@ -4,14 +4,19 @@ Left this comment while using the plugin
 Requirements: Bootstrap and jQuery
 */
 jQuery.fn.extend({
-	tooltipzoom:function() {
-		return this.each(function() {
-		var self=$(this);
-		if(var src=self.attr(src)){
-			self.tooltip({
-				content: "<img src='"+src+"'>"
-			});
+	tooltipzoom:function(){
+		return this.each(function(){
+			var self=$(this);
+			var src=self.attr("src");
+			if(src){
+				self.attr("title","<img style='max-width:90vw;max-height:90vh' src='"+src+"'>");
+				self.tooltip({
+					html:true,
+					placement:'auto right',
+					template:"<div class='tooltip' role='tooltip'><div class='tooltip-arrow'></div><div class='tooltip-inner' style='max-width:95vw;max-height:95vh;'></div></div>"
+				});
+			}
 		});
-	});
+	}
 });
-$(function(){$( ".tooltipzoom" ).each(function(){this.tooltipzoom();});});
+$(function(){$( ".tooltipzoom" ).each(function(){$(this).tooltipzoom();});});
